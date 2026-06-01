63d45f53746fa0596dba863a6e9a50e27b0b6180
Daniil <bksbank@bk.ru>
2026-06-01 18:09:04 +0300

    V1.1

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ logger = logging.getLogger(__name__)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://shop.bytewizard.ru",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
